1e39c356358a1e9b3782c0d323ae0d0f70f173cf
菜小刀 <caixiaodao1997@outlook.com>
2020-10-11 08:53:01 +0800

    update GSL.sh.

--- GSL.sh
+++ GSL.sh
@@ -15,25 +15,24 @@ parse_json(){
 echo "${1//\"/}" | sed "s/.*$2:\([^,}]*\).*/\1/"
 }
 
-
 #__登陆__
-logger -t "【光速联提速脚本】" "----登陆中----"
+logger -t "【光速联提速脚本】" "————————登陆中————————"
 loginInfo=`curl -s -H "Content-Type: application/json" -X POST -d ${LOGIN_DATA} "https://www.fangyb.com:2039/biz/user/login.do"`
 loginCode=$(parse_json ${loginInfo} "code")
 Auth=$(parse_json ${loginInfo} "data")
 if [ 0 == ${loginCode} ]
 then
-    logger -t "【光速联提速脚本】" "----登陆成功----"
+    logger -t "【光速联提速脚本】" "————————登陆成功————————"
 elif [ 12 == ${loginCode} ]
 then
-    logger -t "【光速联提速脚本】" "----用户名或密码错误----"
+    logger -t "【光速联提速脚本】" "————————用户名或密码错误————————"
     exit 0
 elif [ 11 == ${loginCode} ]
 then
-    logger -t "【光速联提速脚本】" "----未注册，请检查手机号是否正确或前往注册后再次运行----"
+    logger -t "【光速联提速脚本】" "————————未注册，请检查手机号是否正确或前往注册后再次运行————————"
     exit 0
 else
-    logger -t "【光速联提速脚本】" "----登录失败，请重试----"
+    logger -t "【光速联提速脚本】" "————————登录失败，请重试————————"
     exit 0
 fi
 
@@ -53,14 +52,14 @@ sleep 1
 
     if [ "true" == ${stateCode} ]
     then
-        logger -t "【光速联提速脚本】" "----提速状态：提速中----"
-        logger -t "【光速联提速脚本】" "----正在重新开始提速----"
+        logger -t "【光速联提速脚本】" "————————提速状态：提速中————————"
+        logger -t "【光速联提速脚本】" "————————正在重新开始提速————————"
     #__关闭提速__
         curl -s -o /dev/null -H "Content-Type: application/json" -H "Authorization: ${Auth}" -X POST -d ${SPEED_DATA} "https://www.fangyb.com:2039/biz/common/closeSpeed.action"
         sleep 2
     else
-        logger -t "【光速联提速脚本】" "----提速状态：未提速----"
-        logger -t "【光速联提速脚本】" "----正在开始提速----"
+        logger -t "【光速联提速脚本】" "————————提速状态：未提速————————"
+        logger -t "【光速联提速脚本】" "————————正在开始提速————————"
     fi
     
     #__开始提速__
@@ -76,7 +75,7 @@ sleep 1
     resultCode=`echo "${speedQuery}" | awk -F  '"' '{print $(NF-1)}'`
     if [ "true" == ${resultCode} ]
     then
-        logger -t "【光速联提速脚本】" "----提速成功----"
+        logger -t "【光速联提速脚本】" "————————提速成功————————"
         echo "`echo "${speedQuery}" | awk -F  ',' '{for (i=6;i<=10;i++){print $i}}'`"
         if [ -n "SCKEY" ]
         then
@@ -89,7 +88,7 @@ sleep 1
         fi
         break
     else
-        logger -t "【光速联提速脚本】" "----提速失败，开始重试"
+        logger -t "【光速联提速脚本】" "————————提速失败，开始重试————————"
         let "NUMBER++"
         #重试次数超过10次，则退出
         if [ ${NUBMER} > 10 ]
